5252145cad700327762b75d09001527b1a223d90
tawatchai.w <tawatchai.w@nextcorp.co.th>
2018-10-25 08:44:07 +0700

    update routes

--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
     var ctrl = require('../controllers/report.controller');
     app.get('/rpt01', ctrl.rpt01);
-    app.get('/test', ctrl.test);
+    // app.get('/test', ctrl.test);
 }
