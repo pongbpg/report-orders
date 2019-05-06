cdcd3df3a040d8775bcfc4149c01432340ec7cd8
tawatchai.w <tawatchai.w@nextcorp.co.th>
2019-05-06 15:46:42 +0700

    route /api/topslim

---
+++ app/routes/topslim.routes.js
@@ -0,0 +1,15 @@
+module.exports = function (app) {
+    var ctrl = require('../controllers/topslim.controller');
+    app.get('/delivery', ctrl.delivery);
+    app.get('/dailyTrack', ctrl.dailyTrack);
+    app.get('/dailySayHi', ctrl.dailySayHi);
+    // app.get('/dailySale', ctrl.dailySale);
+    app.get('/dailyProduct', ctrl.dailyProduct);
+    app.get('/dailyBank', ctrl.dailyBank);
+    app.get('/dailyStatement', ctrl.dailyStatement);
+    app.get('/dailyCost', ctrl.dailyCost);
+    app.get('/dailyChannel', ctrl.dailyChannel);
+    app.get('/com/admin', ctrl.comAdmin);
+    app.get('/item/admin', ctrl.itemAdmin);
+    // app.get('/test', ctrl.updateRt);
+}
