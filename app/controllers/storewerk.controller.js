d26dac36a1763be80835903da8edc362556ea7e1
tawatchai.w <pong.tw34@gmail.com>
2020-02-22 12:09:01 +0700

    invoice

--- app/controllers/storewerk.controller.js
+++ app/controllers/storewerk.controller.js
@@ -1,4 +1,24 @@
 const db = require('../../config/mariadb').db;
+exports.invoiceBymonth = (req, res) => {
+    db.query('call getIntvoiceByMonth(?,?)', [req.params.year, req.params.month])
+        .then(rows => {
+            const data = rows[0] || [];
+            if (data.length > 0) {
+                // res.json(data)
+                // db.query('select accountLogo from accounts where accountId=?', [data[0].accountId])
+                //     .then(rows2 => {
+                //         const LOGO64 = rows2[0].accountLogo.replace('data:image/png;base64,', '');
+                res.ireport("storewerk/invoice.jasper", req.query.file || "pdf", data, {
+                    OUTPUT_NAME: 'receipts',
+                    IS_COPY: req.query.copy || "Y",
+                    // LOGO64
+                });
+                // })
+            } else {
+                res.json(null)
+            }
+        })
+}
 exports.invoice = (req, res) => {
     db.query('call getOrderById(?)', [req.query.orderId])
         .then(rows => {
--- app/routes/storewerk.routes.js
+++ app/routes/storewerk.routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var ctrl = require('../controllers/storewerk.controller');
 
+    app.get('/invoice/:year/:month', ctrl.invoiceBymonth);
     app.get('/invoice', ctrl.invoice);
 }
