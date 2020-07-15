ce56b7dacfcaf6eba9a6728387519cd985c06946
tawatchai.w <pong.tw34@gmail.com>
2020-07-15 12:09:42 +0700

    report add date

--- app/controllers/topslim.controller.js
+++ app/controllers/topslim.controller.js
@@ -2098,7 +2098,7 @@ exports.covid = (req, res) => {
                     admin: doc.data().admin,
                     page: doc.data().page.replace('@', ''),
                     team: doc.data().team,
-                    date: doc.data().orderDate.substring(0, 10)
+                    date: doc.data().orderDate.substring(8, 10)
                 })
             })
             res.json(orders)
